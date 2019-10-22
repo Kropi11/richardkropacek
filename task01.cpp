18516d57b3eff27d3423895bcc4fdec7fd1a1c41
Kropi11 <48060544+Kropi11@users.noreply.github.com>
2019-10-23 00:01:28 +0200

    Update task01.cpp

--- task01.cpp
+++ task01.cpp
@@ -1,7 +1,12 @@
 #include <cstdio>
 
-const char* toUpper(const char* input) {
-	// TO DO
+const char* toUpper(char* input) {
+	for (int i = 0;input[i] != 0;i++){
+		if (input[i] <= 'z' && input[i] >= 'a'){
+			input[i] -= 32;
+		}
+	}
+	return input;
 }
 
 int main() {
