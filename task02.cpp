957540ca52107a045b40276326d325a5c833d49a
Kropi11 <48060544+Kropi11@users.noreply.github.com>
2019-11-26 20:07:15 +0100

    Update task02.cpp

--- task02.cpp
+++ task02.cpp
@@ -12,18 +12,63 @@ name1|name2|...|nameN|
 #define byte unsigned char
 
 byte data[8];
+int bit;
+int inv = 0;
 
-int main() {
-	FILE* fd;
-	errno_t err;
-	err = fopen_s(&fd, "task02_text.txt", "rb");
-	fread(data, 8, 1, fd);
+//otáčení bitů
+	byte flipBits(byte number) {
+		byte inv = 0;
+		for (int i = 0; i < 8; i++)
+		{
+			bit = number >> i;
 
-//jedná se o mou pomlcku, abych viděl jaké hodnoty se nachází naj jednotlivých bytovích kombinacích
-	for(int i = 0; i<8; i++){
-	printf("%d", data[i]);
-	printf("   --->   ");
-	printf("%X %X\n", data[i]);
+			if (bit & 1)
+			{
+				bit = 1;
+			}
+			else
+			{
+				bit = 0;
+
+			}
+		inv |= bit << (7 - i);
 	}
+	return inv;
 }
-//Nevím zcela na jekém principu fungují bitové posuny, a jak je v tomto programu následně implementovat.
+//vypíše bity
+	byte Bits(byte number) {
+		byte inv = 0;
+		for (int i = 7; i >= 0; i--)
+		{
+			bit = number >> i;
+			if (bit & 1)
+			{
+				bit = 1;
+			}
+			else
+			{
+				bit = 0;
+			}
+			inv |= bit << (i);
+		}
+		return inv;
+}
+	void readDate(int* y, int* m, int* d, byte b1, byte b2) {
+		*m = b2 & 0b1111;
+		*d = b2 >> 4 | ((b1 & 0b1) << 4);
+		*y = flipBits(b1 >> 1) >> 1;
+	}
+	int main() {
+		FILE* fd;
+		errno_t err;
+		err = fopen_s(&fd, "task02_text.txt", "rb");
+		
+		fread(data, 8, 1, fd);
+		int y, m, d;
+		readDate(&y, &m, &d, data[0], data[1]);
+		printf("John Doe je narozen: %d.%d.%d\n", d, m, y + 1900);
+		
+		fread(data, 8, 1, fd);
+		readDate(&y, &m, &d, data[0], data[1]);
+		printf("Sam Sepiol je narozen: %d.%d.%d\n", d, m, flipBits(y) + 1900);
+	}
